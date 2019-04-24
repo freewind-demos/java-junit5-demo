dad030b86b1450bef7a1b57843d91c0dc80efb8f
freewind <nowindlee@gmail.com>
2019-04-24 13:13:27 +0800

    demo

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-JUnit 4 Demo
+Java JUnit5 Demo
 ============
 
-JUnit 4 demo code.
+注意：maven（至少<=3.6.0）自带的junit插件不支持junit5。为了让junit5测试正常运行，
+需要在pom.xml中手动添加相应的junit插件。
 
 ```
 mvn test
--- src/test/java/demo/HelloTest.java
+++ src/test/java/demo/HelloTest.java
@@ -1,22 +1,22 @@
 package demo;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
 
 public class HelloTest {
 
     private Hello hello;
 
-    @Before
-    public void setUp() throws Exception {
+    @BeforeEach
+    public void setup() {
         hello = new Hello();
     }
 
     @Test
     public void shouldGreetingWithPassingName() throws Exception {
-        assertEquals(hello.greeting("Freewind"), "Hello, Freewind!");
+        Assertions.assertEquals("Hello, Freewind!", hello.greeting("Freewind"));
     }
 
 }
